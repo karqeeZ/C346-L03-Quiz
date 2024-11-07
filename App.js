6343defbb5a9f6cf6450f44b5bf01777ae60ffe3
Johnson <23009886@myrp.edu.sg>
2024-11-07 19:54:39 +0800

    styling update

--- App.js
+++ App.js
@@ -1,88 +1,118 @@
 import React from 'react';
-import {Text, Alert, StatusBar, Button, ScrollView, StyleSheet} from 'react-native';
+import { Text, Alert, StatusBar, Button, ScrollView, StyleSheet, View } from 'react-native';
 import Question from "./component/question.js";
 import RNPickerSelect from 'react-native-picker-select';
+import Icon from 'react-native-vector-icons/Fontisto';
 
 const App = () => {
     const [answer, setAnswer] = React.useState('');
     const [answer2, setAnswer2] = React.useState('');
     const [answer3, setAnswer3] = React.useState('');
     const [answer4, setAnswer4] = React.useState('');
-  return (
-      <ScrollView>
-          <Text style={styles.text}>Flag Quiz</Text>
-          <StatusBar hidden={true}/>
-          <Question photo={require('./img/cyprus.webp')}  />
-          <RNPickerSelect
-              onValueChange={(value) => setAnswer(value)}
-              items={[
-                  {label:'Cyprus',value:'Cyprus'},
-                  {label:'Saudi Arabia', value:'Saudi Arabia'},
-                  {label:'England', value:'England'}
-              ]}
-          />
-          <Question photo={require('./img/srilanka.webp')}  />
-          <RNPickerSelect
-              onValueChange={(value) => setAnswer2(value)}
-              items={[
-                  {label:'Egypt',value:'Egypt'},
-                  {label:'Sri Lanka', value:'Sri Lanka'},
-                  {label:'Sudan', value:'Sudan'}
-              ]}
-          />
-          <Question photo={require('./img/wales.png')}  />
-          <RNPickerSelect
-              onValueChange={(value) => setAnswer3(value)}
-              items={[
-                  {label:'Albania',value:'Albania'},
-                  {label:'Bhutan', value:'Bhutan'},
-                  {label:'Wales', value:'Wales'}
-              ]}
-          />
-          <Question photo={require('./img/denmark.png')}  />
-          <RNPickerSelect
-              onValueChange={(value) => setAnswer4(value)}
-              items={[
-                  {label:'Iceland',value:'Iceland'},
-                  {label:'Norway', value:'Norway'},
-                  {label:'Denmark', value:'Denmark'}
-              ]}
-          />
 
-          <Button onPress={() => {
-              const correctAnswer = 'Cyprus'
-              const correctAnswer2 = 'Sri Lanka'
-              const correctAnswer3 = 'Wales'
-              const correctAnswer4 = 'Denmark'
-              let message = 'No Answer was entered!'
-              let score = 0
-              if (answer === correctAnswer) {
-                  score += 1;
-              }
-              if (answer2 === correctAnswer2) {
-                  score += 1;
-              }
-              if (answer3 === correctAnswer3) {
-                  score += 1;
-              }
-              if (answer4 === correctAnswer4) {
-                  score += 1;
-              }
-                message = 'You have ' + score +' correct answer(s)!'
-                Alert.alert(message)
-          }}
-                  title="SUBMIT ANSWERS" color='cadetblue'
-          />
-      </ScrollView>
-  )
+    return (
+        <ScrollView style={styles.container}>
+            <Text style={styles.header}>Flags of the World <Icon name={"flag"} size={24} color="white" /></Text>
+            <StatusBar hidden={true} />
+            <View style={styles.questionContainer}>
+                <Question photo={require('./img/cyprus.webp')} />
+                <RNPickerSelect
+                    onValueChange={(value) => setAnswer(value)}
+                    placeholder={{ label: "Select a country", value: null }}
+                    items={[
+                        { label: 'Cyprus', value: 'Cyprus' },
+                        { label: 'Saudi Arabia', value: 'Saudi Arabia' },
+                        { label: 'England', value: 'England' }
+                    ]}
+                />
+            </View>
+            <View style={styles.questionContainer}>
+                <Question photo={require('./img/srilanka.webp')} />
+                <RNPickerSelect
+                    onValueChange={(value) => setAnswer2(value)}
+                    placeholder={{ label: "Select a country", value: null }}
+                    items={[
+                        { label: 'Egypt', value: 'Egypt' },
+                        { label: 'Sri Lanka', value: 'Sri Lanka' },
+                        { label: 'Sudan', value: 'Sudan' }
+                    ]}
+                />
+            </View>
+            <View style={styles.questionContainer}>
+                <Question photo={require('./img/wales.png')} />
+                <RNPickerSelect
+                    onValueChange={(value) => setAnswer3(value)}
+                    placeholder={{ label: "Select a country", value: null }}
+                    items={[
+                        { label: 'Albania', value: 'Albania' },
+                        { label: 'Bhutan', value: 'Bhutan' },
+                        { label: 'Wales', value: 'Wales' }
+                    ]}
+                />
+            </View>
+            <View style={styles.questionContainer}>
+                <Question photo={require('./img/denmark.png')} />
+                <RNPickerSelect
+                    onValueChange={(value) => setAnswer4(value)}
+                    placeholder={{ label: "Select a country", value: null }}
+                    items={[
+                        { label: 'Iceland', value: 'Iceland' },
+                        { label: 'Norway', value: 'Norway' },
+                        { label: 'Denmark', value: 'Denmark' }
+                    ]}
+                />
+            </View>
+
+            <View style={styles.buttonContainer}>
+                <Button onPress={() => {
+                    const correctAnswer = 'Cyprus';
+                    const correctAnswer2 = 'Sri Lanka';
+                    const correctAnswer3 = 'Wales';
+                    const correctAnswer4 = 'Denmark';
+                    let message = 'No Answer was entered!';
+                    let score = 0;
+                    if (answer === correctAnswer) score += 1;
+                    if (answer2 === correctAnswer2) score += 1;
+                    if (answer3 === correctAnswer3) score += 1;
+                    if (answer4 === correctAnswer4) score += 1;
+                    message = 'You have ' + score + ' correct answer(s)!';
+                    Alert.alert(message);
+                }}
+                        title="SUBMIT ANSWERS" color='cadetblue'
+                />
+            </View>
+        </ScrollView>
+    );
 }
 
 const styles = StyleSheet.create({
-    text: {
+    header: {
         textAlign: 'center',
-        fontSize: 30,
-        color: 'black',
-    }
+        fontStyle: 'italic',
+        fontSize: 24,
+        color: 'white',
+        backgroundColor: 'cadetblue',
+        fontWeight: 'bold',
+        padding: 15,
+        marginBottom: 20,
+    },
+    container: {
+        flex: 1,
+        backgroundColor: 'beige',
+    },
+    questionContainer: {
+        backgroundColor: 'white',
+        marginHorizontal: 20,
+        marginVertical: 10,
+        padding: 15,
+        borderRadius: 10,
+        shadowColor: "black",
+        elevation: 5,
+    },
+    buttonContainer: {
+        margin: 20,
+        borderRadius: 10 ,
+    },
 });
 
 export default App;
--- component/question.js
+++ component/question.js
@@ -1,21 +1,41 @@
 import React from 'react';
-import {View,Image,Text,StyleSheet} from 'react-native';
+import { View, Image, Text, StyleSheet } from 'react-native';
 
-const Question = ({photo}) => {
+const Question = ({ photo }) => {
     return (
         <View style={styles.container}>
-            <Image source={photo} style={{height:200,width:400,borderWidth:0.1,borderColor:'black'}}/>
-            <Text>What country does this flag belong to?</Text>
+            <Image source={photo} style={styles.image} />
+            <Text style={styles.question}>Which nation's flag is this?</Text>
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
+        alignItems: 'center',
+        padding: 20,
+        backgroundColor: 'white',
+        borderRadius: 10,
+        shadowColor: "black",
+        elevation: 5,
+    },
+    image: {
+        height: 180,
+        width: 320,
+        marginBottom: 20,
+        borderWidth: 0.6,
+        borderColor: 'cadetblue',
+    },
+    question: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+        borderRadius: 10,
+        backgroundColor: 'cadetblue',
+        textAlign: 'center',
+    },
 });
 
-export default Question
+export default Question;
